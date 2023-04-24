465514c1ce81fe015377292189598ce6efe2f989
Ariesh Grover <41345908+groverari@users.noreply.github.com>
2023-04-23 21:49:56 -0700

    Updates to ui

--- frontend/src/pages/students/student-page/student-page.jsx
+++ frontend/src/pages/students/student-page/student-page.jsx
@@ -3,30 +3,30 @@ import React from 'react'
 import './student-page.styles.scss'
 import SubMenu from '../../../components/submenu/submenu'
 
+const baseurl = '/home/students'
+const links = [
+  {
+    key: 1,
+    name: 'Student Search',
+    path: ''
+  },
+  {
+    key: 2,
+    name: 'Add Student',
+    path: '/add'
+  },
+  {
+    key: 3,
+    name: 'Achived Students',
+    path: '/archived'
+  },
+  {
+    key: 4,
+    name: 'Lost Students',
+    path: '/lost'
+  }
+]
 const StudentPage = () => {
-  const baseurl = '/home/students'
-  const links = [
-    {
-      key: 1,
-      name: 'Student Search',
-      path: ''
-    },
-    {
-      key: 2,
-      name: 'Add Student',
-      path: '/add'
-    },
-    {
-      key: 3,
-      name: 'Achived Students',
-      path: '/archived'
-    },
-    {
-      key: 4,
-      name: 'Lost Students',
-      path: '/lost'
-    }
-  ]
   return (
     <div className="page-container">
       <SubMenu className="submenu" baseurl={baseurl} links={links} />
--- frontend/src/pages/students/student-search/student-search.jsx
+++ frontend/src/pages/students/student-search/student-search.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import './student-search.styles.scss'
+import SearchBox from '../../../components/search-box/search-box'
+import { useState, useEffect } from 'react'
+const s = [
+  { label: 'Ariesh', value: 1 },
+  { label: 'Josh', value: 2 },
+  { label: 'Andrew', value: 3 },
+  { label: 'Nirunjan', value: 4 },
+  { label: 'Ryan', value: 5 },
+  { label: 'John Doe', value: 7 },
+  { label: 'Billy Eilish', value: 8 }
+]
 
 const StudentSearch = () => {
-  return <h1>I am Studetn search page</h1>
+  const [students, setStudents] = useState(0)
+
+  useEffect(() => {
+    setStudents(s)
+    //this is where the fetch for the database will go
+  }, [])
+  if (students) {
+    students.sort(function (a, b) {
+      return a.label.localeCompare(b.label)
+    })
+  }
+
+  return (
+    <SearchBox
+      list={students}
+      placeholder="Search for a Students"
+      value="Search"
+    />
+  )
 }
 
 export default StudentSearch

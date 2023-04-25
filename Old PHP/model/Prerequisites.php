4888790e2ee03691a4b8763497954ece9efb646f
Bausav24 <92281958+Bausav24@users.noreply.github.com>
2023-04-25 16:12:01 -0700

    prerequisites updated!

--- Old PHP/model/Prerequisites.php
+++ Old PHP/model/Prerequisites.php
@@ -1,41 +1,31 @@
 <?php
-class Replacement_Classes {
+include_once 'PDO-methods.php';
 
-    private $conn;
-    private $table ='prerequisites';
-
-    public $id;
-    public $class_id;
-    public $prerequisite_id;
-    public $minimum_grade;
+class Prerequisites {
+    private $db;
+    private $table = 'prerequisites';
 
     public function __construct($db) {
-        $this->conn = $db;
+        $this->db = $db;
     }
 
-    public function update_prereqs($class_id, $prereq_ids, $required_grades) {
+    public function updatePrerequisites($class_id, $prereq_ids, $required_grades) {
         try {
             // Delete existing prerequisites for the given class
-            $stmt = $this->conn->prepare("DELETE FROM Prerequisites WHERE class_id = :class_id");
-            $stmt->bindParam(":class_id", $class_id);
-            $stmt->execute();
+            $query = "DELETE FROM " . $this->table . " WHERE class_id = ?";
+            $this->db->add_db($query, [$class_id]);
 
             // Insert new prerequisites for the given class
-            foreach($prereq_ids as $prereq_id) {
-                $stmt = $this->conn->prepare("INSERT INTO Prerequisites (class_id, prerequisite_id) VALUES (:class_id, :prereq_id)");
-                $stmt->bindParam(":class_id", $class_id);
-                $stmt->bindParam(":prereq_id", $prereq_id);
-                $stmt->execute();
+            foreach ($prereq_ids as $prereq_id) {
+                $query = "INSERT INTO " . $this->table . " (class_id, prerequisite_id) VALUES (?, ?)";
+                $this->db->add_db($query, [$class_id, $prereq_id]);
             }
 
             // Insert new prerequisites with minimum grade for the given class
             foreach ($required_grades as $prereq_id => $minimum_grade) {
                 if ($minimum_grade > 0) {
-                    $stmt = $this->conn->prepare("INSERT INTO Prerequisites (class_id, prerequisite_id, minimum_grade) VALUES (:class_id, :prereq_id, :minimum_grade)");
-                    $stmt->bindParam(":class_id", $class_id);
-                    $stmt->bindParam(":prereq_id", $prereq_id);
-                    $stmt->bindParam(":minimum_grade", $minimum_grade);
-                    $stmt->execute();
+                    $query = "INSERT INTO " . $this->table . " (class_id, prerequisite_id, minimum_grade) VALUES (?, ?, ?)";
+                    $this->db->add_db($query, [$class_id, $prereq_id, $minimum_grade]);
                 }
             }
             return true; // Return true if update is successful
@@ -44,22 +34,18 @@ class Replacement_Classes {
         }
     }
 
-    public function get_prereqs($class_id) {
+    public function getPrerequisites($class_id) {
         try {
             // Select prerequisites and their minimum grades for the given class
-            $stmt = $this->conn->prepare("SELECT Prerequisites.prerequisite_id, Classes.name, Prerequisites.minimum_grade FROM Prerequisites JOIN Classes ON Prerequisites.prerequisite_id=Classes.id WHERE Prerequisites.class_id = :class_id");
-            $stmt->bindParam(":class_id", $class_id);
-            $stmt->execute();
-            
+            $query = "SELECT " . $this->table . ".prerequisite_id, Classes.name, " . $this->table . ".minimum_grade FROM " . $this->table . " JOIN Classes ON " . $this->table . ".prerequisite_id = Classes.id WHERE " . $this->table . ".class_id = ?";
+            $prereqs = $this->db->get_from_db($query, [$class_id]);
+
             // Return an array of prerequisites and their minimum grades
-            $prereqs = array();
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                $prereqs[$row['prerequisite_id']] = $row;
-            }
             return $prereqs;
         } catch(PDOException $e) {
             return false; // Return false if there is an error
         }
     }
 }
+
 

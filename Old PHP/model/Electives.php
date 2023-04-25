cbfb8b6e778e9d1f9e079bfdbcb065bb663175e3
Bausav24 <92281958+Bausav24@users.noreply.github.com>
2023-04-25 16:55:53 -0700

    electives.php updated (model)

---
+++ Old PHP/model/Electives.php
@@ -0,0 +1,51 @@
+<?php
+include_once 'PDO-methods.php';
+
+class Electives 
+{
+    private $db;
+    private $table = 'electives';
+
+    public function __construct($db) 
+    {
+        $this->db = $db;
+    }
+
+    public function get_electives_credits($student_id, $program_id)
+    {
+        $query_string = "
+        SELECT
+            Classes.id AS class_id,
+            Classes.name AS short_name,
+            CONCAT(Classes.name, ' (', Classes.credits, ' cr)') AS name,
+            Classes.title,
+            Classes.credits,
+            Classes.fall,
+            Classes.winter,
+            Classes.spring,
+            Classes.summer,
+            Student_Classes.term,
+            Student_Classes.id,
+            Electives.id AS elective_id
+        FROM
+            Electives
+            JOIN Student_Classes ON Electives.student_class_id=Student_Classes.id
+            JOIN Classes ON Student_Classes.class_id = Classes.id
+        WHERE
+            Student_Classes.student_id = ?
+            AND
+            Electives.program_id = ?
+        ;";
+        $query_result = $this->db->get_from_db($query_string, [$student_id, $program_id]);
+
+        $credits = 0;
+        $electives = array();
+        foreach ($query_result as $row)
+        {
+            $electives[$row['id']] = $row;
+            $credits += $row['credits'];
+        }
+
+        return array('electives' => $electives, 'credits' => $credits);
+    }
+}

05aa530d532289440cb5dab52bd75e67bd2c9e78
Ariesh Grover <41345908+groverari@users.noreply.github.com>
2023-04-26 15:11:56 -0700

    login page

--- frontend/src/pages/login-page/login.page.jsx
+++ frontend/src/pages/login-page/login.page.jsx
@@ -1,27 +1,34 @@
 import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
+import './login-page.styles.scss'
+
 const Login = () => {
-  console.log('im in the login page')
   return (
-    <>
-      <div className="login-username-container">
-        <label className="username-label">Username</label>
-        <input placeholder="username" className="username-field" type="text" />
+    <div className="login-page">
+      <div className="login-container">
+        <div className="login-username-container">
+          <label className="username-label">Username</label>
+          <input
+            placeholder="username"
+            className="username-field"
+            type="text"
+          />
+        </div>
+        <div className="password-contianer">
+          <label className="password-label">Password</label>
+          <input
+            className="password-field"
+            placeholder="password"
+            type="password"
+          />
+        </div>
+        <div className="login-button-container">
+          <Link to="/home">
+            <button className="login-button">Login</button>
+          </Link>
+        </div>
       </div>
-      <div className="password-contianer">
-        <label className="password-label">Password</label>
-        <input
-          className="password-field"
-          placeholder="password"
-          type="password"
-        />
-      </div>
-      <div className="login-button-container">
-        <Link to="/home">
-          <button className="login-button">Login</button>
-        </Link>
-      </div>
-    </>
+    </div>
   )
 }
 

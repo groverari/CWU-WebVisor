a3b6eb633f55552bf78910fde14cf1192111a338
Bausav24 <92281958+Bausav24@users.noreply.github.com>
2023-04-25 23:04:29 -0700

    Added files in API folder.
    so far they only have headers.

---
+++ Old PHP/API/Prerequisites.php
@@ -0,0 +1,10 @@
+<?php
+    //headers
+    
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+    
+    include_once '../config/Database.php';
+    include_once '../model/Prerequisites.php';

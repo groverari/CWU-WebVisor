0aa3b7f84157c6011a782b027d2533a4f8374cf6
Ariesh Grover <41345908+groverari@users.noreply.github.com>
2023-04-27 09:56:40 -0700

    login page

--- frontend/src/pages/login-page/login.page.jsx
+++ frontend/src/pages/login-page/login.page.jsx
@@ -1,21 +1,24 @@
 import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
+import { PasswordField } from '@aws-amplify/ui-react'
 import './login-page.styles.scss'
+import '@aws-amplify/ui-react/styles.css'
 
 const Login = () => {
   return (
     <div className="login-page">
+      <h1 className="login-title">WebVisor</h1>
       <div className="login-container">
-        <div className="login-username-container">
-          <label className="username-label">Username</label>
+        <div className="login-username-container login-box">
+          <label className="username-label login-label">Username</label>
           <input
             placeholder="username"
             className="username-field"
             type="text"
           />
         </div>
-        <div className="password-contianer">
-          <label className="password-label">Password</label>
+        <div className="password-contianer login-box">
+          <label className="password-label login-label">Password</label>
           <input
             className="password-field"
             placeholder="password"

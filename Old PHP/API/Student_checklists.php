eb548a5cd819f99d9c206c4e8195b6fc901d50a7
Bausav24 <92281958+Bausav24@users.noreply.github.com>
2023-04-30 13:11:31 -0700

    Update Student_checklists.php
    function added to API (calling url)

--- Old PHP/API/Student_checklists.php
+++ Old PHP/API/Student_checklists.php
@@ -8,3 +8,47 @@
     
     include_once '../config/Database.php';
     include_once '../model/Student_checklists.php';
+
+    $student_checklist = new $Student_checklists;
+
+    $request;
+
+    //properties of table
+    $id;
+    $student_id;
+    $checklist_id;
+
+    //checks url for request variable
+
+    if(isset($_GET['id']))
+    {
+        $id = $_GET['id'];
+    }
+    if(isset($_GET['student_id']))
+    {
+        $student_id = $_GET['student_id'];
+    }
+    if(isset($_GET['checklist_id']))
+    {
+        $checklist_id = $_GET['checklist_id'];
+    }
+
+    //calls funtions based on funtion 
+    switch($request){
+
+        case 'clearchecklist':
+            $result = $student_checklist->clearchecklist($user_id, $student_id, $program_id);
+            break;
+        case 'checkchecklist':
+            $result = $student_checklist->checkchecklist($user_id, $student_id, $checklist_id);
+            break;
+        case 'updatechecklist':
+            $result = $student_chekcklist ->updatechecklist($user_id, $student_id, $program_id, $checklist_ids);
+            break;
+        case 'get_checked_items':
+            $result = $student_checklist->get_checked_items($user_id, $student_id, $program_id);
+            break;
+        default:
+            echo 'request incorrrect';
+            break;
+    }

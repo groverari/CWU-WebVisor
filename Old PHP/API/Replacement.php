e0120c69646fef07322673d2f2c24900a22bdb48
Bausav24 <92281958+Bausav24@users.noreply.github.com>
2023-04-30 12:24:27 -0700

    replacement.php(API)
    url request on replacement.php

--- Old PHP/API/Replacement.php
+++ Old PHP/API/Replacement.php
@@ -8,3 +8,67 @@
     
     include_once '../config/Database.php';
     include_once '../model/Replacement.php';
+
+    $replacement = new $Replacement;
+
+    $request;
+
+    //properties of table;
+
+    $id;
+    $program_id;
+    $required_id;
+    $replacement_id;
+    $note;
+
+    //checks url for table
+    if(isset($_GET['id']))
+    {
+        $id = $_GET['id'];
+    }
+    if(isset($_GET['program_id']))
+    {
+        $program_id = $_GET['program_id'];
+    }
+    if(isset($_GET['required_id']))
+    {
+        $required_id = $_GET['required_id'];
+    }
+    if(isset($_GET['replacement_id']))
+    {
+        $replacement_id = $_GET['replacement_id'];
+    }
+    if(isset($_GET['note']))
+    {
+        $note = $_GET['note'];
+    }
+
+
+       
+
+        //checks url for request variable
+    if(isset($_GET['request']))
+    {
+        $request = $_GET['request'];
+    }
+
+
+    switch($request){
+        
+        case 'addReplacement':
+            $result = $replacement ->addReplacement($user_id, $program_id, $replaced_id, $replacement_id);
+            break;
+        case 'removeReplacement':
+            $result = $replacement ->removeReplacement($user_id, $program_id, $replaced_id, $replacement_id);
+            break;
+        case 'getReplacementClasses':
+            $result = $replacement ->getReplacement($program_id);
+            break;
+        case 'record_update_program':
+            $result = $replacement -> record_update_program($user_id, $program_id, $note);
+            break;
+        default:
+            echo 'request incorrrect';
+            break;
+    
+    }

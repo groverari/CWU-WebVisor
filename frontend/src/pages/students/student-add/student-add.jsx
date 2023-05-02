03bf36ade3d94261e413265b6d0ca6788fd8a223
Ariesh Grover <41345908+groverari@users.noreply.github.com>
2023-05-02 12:38:48 -0700

    help for nirunjan

--- frontend/src/pages/students/student-add/student-add.jsx
+++ frontend/src/pages/students/student-add/student-add.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 import './student-add.scss'
 
 const AddStudent = () => {
+  /**
+   * Change handler
+   * check data for validity
+   * retrieve info from form
+   * turn data into json
+   * send to database
+   * wait for confirmation
+   * if success show green banner
+   * else show error popup
+   */
   return <h1>I am Student Add page</h1>
+  /**
+   * Write HTML for form and button
+   */
 }
 
 export default AddStudent

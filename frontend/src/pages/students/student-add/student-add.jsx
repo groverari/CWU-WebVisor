cf638c7084bfdec0f50d285320f97716dda5cb9b
Bausav24 <92281958+Bausav24@users.noreply.github.com>
2023-05-02 22:27:35 -0700

    Update student-add.jsx

--- frontend/src/pages/students/student-add/student-add.jsx
+++ frontend/src/pages/students/student-add/student-add.jsx
@@ -1,27 +1,8 @@
-import React from 'react'
-import './student-add.scss'
+import React from "react";
+import "./student-add.scss";
 
 const AddStudent = () => {
-<<<<<<< HEAD
-  return (
-    <h1>Add Student Page</h1>
-  )
-=======
-  /**
-   * Change handler
-   * check data for validity
-   * retrieve info from form
-   * turn data into json
-   * send to database
-   * wait for confirmation
-   * if success show green banner
-   * else show error popup
-   */
-  return <h1>I am Student Add page</h1>
-  /**
-   * Write HTML for form and button
-   */
-//>>>>>>> 03bf36ade3d94261e413265b6d0ca6788fd8a223
-}
+  return <h1>Add Student Page</h1>;
+};
 
-export default AddStudent
+export default AddStudent;

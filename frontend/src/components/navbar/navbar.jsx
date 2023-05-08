03dd08df545ef826f287fe0870915a0d0045baf4
Rifintidhamar <rifintidhamar@gmail.com>
2023-05-07 18:26:33 -0700

    clicked submenu

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -9,9 +9,10 @@ const NavBar = () => {
 
   const [activeLink, setActiveLink] = useState(null);
 
-  const handleLinkClick = (link) => {
+  const handleLinkClick = (link) => 
+  {
     setActiveLink(link);
-    
+
   };
 
   const clickedStyle ={
@@ -19,7 +20,7 @@ const NavBar = () => {
   }
 
   const unclickedStyle ={
-    backgroundColor: 'white',
+    backgroundColor: '#b092ff',
     color: 'black'
   }
 
@@ -56,88 +57,4 @@ const NavBar = () => {
 }
 export default NavBar
 
-// import { Link, Outlet } from 'react-router-dom'
-// import { GiGears } from 'react-icons/gi'
-// import React, { useState } from 'react';
-// import '../../App.scss';
-// import './navbar.styles.scss';
-
-// const NavBar = () => {
-
-//   const [activeLink, setActiveLink] = useState(null);
-
-//   const handleLinkClick = (link) => {
-//     setActiveLink(link);
-//   };
-
-//   const linkStyle = {
-//     width: '25%',
-//     backgroundColor: 'button-color',
-//     textAlign: 'center',
-//     margin: '2px',
-//     borderRadius: '12px',
-//     fontSize: '20px',
-//     textDecoration: 'none',
-//     color: 'black', 
-//     border: '2px solid black',
-//     paddingTop: '10px',
-//     transitionTimingFunction: 'ease-in',
-//     transition: 'scale, font-size 0.2s'
-//   };
-
-//   const activeLinkStyle = {
-//     backgroundColor: 'blue', 
-//     color: 'black', 
-//     fontSize: '22px',
-//     transform: 'scale(1.05)'
-//   };
-
-//   return (
-//     <>
-//       <div className="header">
-//         <div className="title-container">
-//           <h1 className="title">WebVisor</h1>
-//         </div>
-
-//         <div className="username-container">
-//           <h2 className="username">Hi Ariesh</h2>
-//           <GiGears className="settings-logo" />
-//         </div>
-//       </div>
-//       <div className="nav-link-container">
-//         <Link
-//           style={activeLink === 'students' ? { ...linkStyle, ...activeLinkStyle } : linkStyle}
-//           to="/home/students/search"
-//           onClick={() => handleLinkClick('students')}
-//         >
-//           Students
-//         </Link>
-//         <Link
-//           style={activeLink === 'class' ? { ...linkStyle, ...activeLinkStyle } : linkStyle}
-//           to="/home/class"
-//           onClick={() => handleLinkClick('class')}
-//         >
-//           Classes
-//         </Link>
-//         <Link
-//           style={activeLink === 'major' ? { ...linkStyle, ...activeLinkStyle } : linkStyle}
-//           to="/home/major"
-//           onClick={() => handleLinkClick('major')}
-//         >
-//           Majors
-//         </Link>
-//         <Link
-//           style={activeLink === 'enrollments' ? { ...linkStyle, ...activeLinkStyle } : linkStyle}
-//           to="/home/enrollments"
-//           onClick={() => handleLinkClick('enrollments')}
-//         >
-//           Enrollments
-//         </Link>
-//       </div>
-//       <Outlet />
-//     </>
-//   );
-// }
-
-// export default NavBar;
 
--- frontend/src/components/submenu/submenu.jsx
+++ frontend/src/components/submenu/submenu.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import './submenu.styles.scss'
 
@@ -6,12 +6,19 @@ const SubMenu = (props) => {
   const baseurl = props.baseurl
   const { links } = props
 
+  const [activeLink, setActiveLink] = useState(null);
+
+  const handleLinkClick = (link) => 
+  {
+    setActiveLink(link);
+  };
+
   const clickedStyle ={
     backgroundColor: '#745cab',
   }
 
   const unclickedStyle ={
-    backgroundColor: 'white',
+    backgroundColor: '#b092ff',
     color: 'black'
   }
   return (
@@ -19,7 +26,7 @@ const SubMenu = (props) => {
       {links.map((link) => {
         return (
           <div className="submenu-link-container" key={link.key}>
-            <Link className="submenu-link" to={baseurl + link.path}>
+            <Link className={`submenu-link ${activeLink === link.name ? 'active':''}`} to={baseurl + link.path} onClick={() => handleLinkClick(link.name)} style={activeLink === link.name ? { ...clickedStyle, ...unclickedStyle } : clickedStyle}>
               {link.name}
             </Link>
           </div>

d5403e830a7d6ff4af2ef7cf775a6a83d75f5edc
Bausav24 <92281958+Bausav24@users.noreply.github.com>
2023-05-09 12:55:22 -0700

    added prettier
    a

--- frontend/src/pages/classes/class-search/class-search.jsx
+++ frontend/src/pages/classes/class-search/class-search.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import './class-search.styles.scss'
+import React from "react";
+import "./class-search.styles.scss";
+import SearchBox from "../../../components/search-box/search-box";
+import { useState, useEffect } from "react";
 
 const ClassSearch = () => {
-  return <h1>This is Class Search Page</h1>
-}
+  return <h1>This is Class Search Page</h1>;
+};
 
-export default ClassSearch
+export default ClassSearch;

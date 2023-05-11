e897cc2fd828475589e0c5817124d5431f9a0671
Ariesh Grover <41345908+groverari@users.noreply.github.com>
2023-05-10 23:17:55 -0700

    plan is kinda working

--- frontend/src/components/plan-class-select-box/plan-class-select-box.jsx
+++ frontend/src/components/plan-class-select-box/plan-class-select-box.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import './plan-class-select-box.styles.scss'
 
 function PlanSelectBox({ classes }) {
   return (
     <div>
-      <select>
+      <select className="select-box">
+        <option value={0}>-- Select a Class --</option>
         {classes.map((clss) => (
-          <option value={clss.id}>{clss.name}</option>
+          <option key={clss.id} value={clss.id}>
+            {clss.name}
+          </option>
         ))}
       </select>
     </div>

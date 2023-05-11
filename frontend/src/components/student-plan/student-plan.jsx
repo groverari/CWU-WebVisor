ed4941031fd1285aaa98bdfcaab9a51426ed3606
Ariesh Grover <41345908+groverari@users.noreply.github.com>
2023-05-10 19:37:18 -0700

    moved class selector

--- frontend/src/components/student-plan/student-plan.jsx
+++ frontend/src/components/student-plan/student-plan.jsx
@@ -5,20 +5,44 @@ import axios from 'axios'
 import ClassSelector from '../class-selector/class-selector'
 
 function StudentPlan() {
-  const [classes, setClasses] = useState(0)
+  const [startYear, setStartYear] = useState(0)
+  const [endYear, setEndYear] = useState(0)
+
+  //Code here to set start year if the database has an earlier start year
+  setStartYear(2023)
+
+  // Code here to set end year as the maximum of last end year
+  // found in the database or the start year + 4
+
+  setEndYear(2027)
+
   /*
+  const [classes, setClasses] = useState(0)
   const [fallClasses, setFall] = useState(0)
   const [winterClasses, setWinter] = useState(0)
   const [springClasses, setSpring] = useState(0)
   const [summerClasses, setSummer] = useState(0)
-*/
+
   let api_url = import.meta.env.VITE_API_URL
   useEffect(() => {
     axios
       .post(api_url + 'Class.php', { request: 'all_active_classes' })
       .then((res) => setClasses(res.data))
   }, [])
-  return <ClassSelector title="test" classes={classes} />
+  */
+  return (
+    <div>
+      <h3>Plan</h3>
+      <div className="year-container">
+        {/**drop down to denote the start and end year  */}
+      </div>
+      <div className="plan-class-container">
+        {/**component to create a list of classes
+         * need to use an array
+         */}
+      </div>
+    </div>
+  )
 }
 
 export default StudentPlan
--- frontend/src/pages/classes/class-search/class-search.jsx
+++ frontend/src/pages/classes/class-search/class-search.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import "./class-search.styles.scss";
-import SearchBox from "../../../components/search-box/search-box";
-import { useState, useEffect } from "react";
+import React from 'react'
+import './class-search.styles.scss'
+import SearchBox from '../../../components/search-box/search-box'
+import { useState, useEffect } from 'react'
 
 const ClassSearch = () => {
-  return <h1>This is Class Search Page</h1>;
-};
+  const [classes, setClasses] = useState(0)
+  let api_url = import.meta.env.VITE_API_URL
+  useEffect(() => {
+    axios
+      .post(api_url + 'Class.php', { request: 'all_active_classes' })
+      .then((res) => setClasses(res.data))
+  }, [])
+  return <ClassSelector title="test" classes={classes} />
+}
 
-export default ClassSearch;
+export default ClassSearch

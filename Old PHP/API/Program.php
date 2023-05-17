b4de99df5d416a86fba17b1b1ebbeccb65d851e2
Rifintidhamar <rifintidhamar@gmail.com>
2023-05-16 22:19:51 -0700

    some changes (add program bugged)

--- Old PHP/API/Program.php
+++ Old PHP/API/Program.php
@@ -9,89 +9,40 @@
 
     $programs = new Programs();
 
-    $request;
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
 
-    //attributes of the tables
-    $id;
-    $major_id;
-    $year;
-    $credits;
-    $elective_credits;
-    $active;
+    //validate api key before moving forward
 
-    //needed attributes for some functions
-    $catalog_year;
-    $user_id;
-    $template_id;
-
-     //checks url for table variables
-     if(isset($_GET['id']))
-     {
-         $id = $_GET['id'];
-     }
-     if(isset($_GET['major_id']))
-     {
-         $major_id = $_GET['major_id'];
-     }
-     if(isset($_GET['year']))
-     {
-         $year = $_GET['year'];
-     }
-     if(isset($_GET['credits']))
-     {
-         $credits = $_GET['credits'];
-     }
-     if(isset($_GET['elective_credits']))
-     {
-         $elective_credits = $_GET['elective_credits'];
-     }
-     if(isset($_GET['active']))
-     {
-         $active = $_GET['active'];
-     }
-     if(isset($_GET['catalog_year']))
-     {
-         $catalog_year = $_GET['catalog_year'];
-     }
-     if(isset($_GET['user_id']))
-     {
-         $user_id = $_GET['user_id'];
-     }
-     if(isset($_GET['template_id']))
-     {
-         $template_id = $_GET['template_id'];
-     }
-     
-     //checks url for request variable
-     if(isset($_GET['request']))
-     {
-         $request = $_GET['request'];
-     }
+    $request = $data['request'];
 
      //calls function based on url request
     switch ($request) 
     {
         case 'get_program_id':
-            $result = $programs->get_program_id($major_id, $catalog_year);
-            echo json_encode($result);
+            // $result = $programs->get_program_id($major_id, $catalog_year);
+            // echo json_encode($result);
             break;
         case 'all_programs':
-            $result = $programs->all_programs($user_id);
+            $result = $programs->all_programs($data['user_id']);
             echo json_encode($result);
             break;
         case 'get_program_info':
-            $result = $programs->get_program_info($id);
-            echo json_encode($result);
+            // $result = $programs->get_program_info($id);
+            // echo json_encode($result);
             break;
         case 'get_program_roster':
-            $result = $programs->get_program_roster($id);
-            echo json_encode($result);
+            // $result = $programs->get_program_roster($id);
+            // echo json_encode($result);
             break;
         case 'add_program':
-            $programs->add_program($user_id, $major_id, $year, $template_id);
+            //$programs->add_program($user_id, $major_id, $year, $template_id);
             break;
-        case 'update_programs':
-            $programs->update_program($user_id, $id, $major_id, $year, $credits, $elective_credits, $active);
+        case 'update':
+            $result = $programs->update_program($data['user_id'], $data['id'], $data['major_id'], $data['year'], $data['credits'], $data['elective_credits'], $data['active']);
+            echo json_encode($result);
+            break;
+            //$programs->update_program($user_id, $id, $major_id, $year, $credits, $elective_credits, $active);
         default:
             echo 'request incorrrect';
             break;
--- frontend/src/pages/majors/program-edit/program-edit.jsx
+++ frontend/src/pages/majors/program-edit/program-edit.jsx
@@ -1,8 +1,221 @@
-import React from 'react'
-import './program-edit.styles.scss'
+import React, { useState, useEffect } from 'react'
+//import './major-edit.styles.scss'
+//import MajorInfo from '../../../components/major-info/major-info'
+import SearchBox from '../../../components/search-box/search-box'
+import axios from 'axios'
+import ConfPopUp from '../../../components/PopUp/confirmation/confPopUp'
+import ErrorPopUp from '../../../components/PopUp/error/errorPopUp'
+//import { program } from '@babel/types'
 
+/**
+ *
+ *
+ * TODO: Generate an Error Popup that says
+ */
 function EditProgram() {
-  return <h1>This is the Edit Program Page</h1>
+  const api_url = import.meta.env.VITE_API_URL
+  const [programs, setPrograms] = useState([])
+  const [searchPrograms, setSearchPrograms] = useState([])
+  const [selectedProgram, setSelectedProgram] = useState([])
+  const [showInfo, setInfo] = useState(false)
+  const [errorMessage, setErrorMesssage] = useState('');
+  const [showError, setShowError] = useState(false);
+
+ 
+
+  const handleErrorPopUpClose = () =>
+  {
+    setShowError(false);
+    //window.location.reload(true);
+  }
+
+  useEffect(() => {
+    axios
+      .post(api_url + 'Program.php', {
+        request: 'all_programs',
+        user_id: localStorage.getItem('userId')
+      })
+      .then((res) => {
+        setPrograms(res.data)
+        console.log(res.data)
+      })
+  }, [])
+
+  // const {
+  //   major_id,
+  //   year,
+  //   credits, 
+  //   elective_credits,
+  //   active
+  // } = programs
+  // console.log(major_id);
+
+  useEffect(() => {
+    if (programs) {
+      const temp = programs.map((program) => ({
+        label: program.name,
+        value: programs.indexOf(program)
+      }))
+      setSearchPrograms(temp)
+    }
+  }, [programs])
+
+  if (searchPrograms) {
+    searchPrograms.sort(function (a, b) {
+      return a.label.localeCompare(b.label)
+    })
+  }
+
+  const selectHandler = ({ value }) => {
+    setName('')
+    setSelectedProgram(programs[value])
+    setInfo(false)
+  }
+
+  const deactivator = () => {
+    console.log('To be deleted')
+  }
+
+  const buttonHandler = () => {
+    setInfo(true)
+  }
+  const updator = () => {
+    if (updatedName == '' || selectedProgram.name == updatedName) {
+      console.log('No Changes Yet')
+    } else {
+      handlePopUpOpen()
+    }
+  }
+
+  const handleUpdate = () => {
+    delete programs[programs.indexOf(selectedProgram)]
+    selectedProgram.name = updatedName
+    setPrograms(programs.concat(selectedProgram))
+    console.log(programs)
+    console.log(searchPrograms)
+    axios.post(api_url + 'Program.php', {
+      request: 'update',
+      user_id: localStorage.getItem('userId'),
+      id: selectedProgram.id,
+      major_id: updatedName,
+      active: 'Yes'
+    })
+   .then((res) => {
+      console.log(res.data)
+      if(typeof res.data === 'string' && res.data.includes('Error'))
+      {
+        console.log("handled error");
+        setErrorMesssage(res.data);
+        setShowError(true);
+      }
+      else
+      {
+        window.location.reload(true);
+        console.log("no error")
+      }
+    })
+  .catch((error)=>
+    {
+      console.log(error);
+    })
+  }
+  // Code to generate Popup
+  const [showPopup, setShowPopup] = useState(false)
+  const [selectedOption, setSelectedOption] = useState(null)
+
+  const handlePopUpOpen = () => {
+    setShowPopup(true)
+  }
+
+  const handlePopUpClose = () => {
+    setShowPopup(false)
+  }
+
+  const handlePopUpButtonClick = (buttonValue) => {
+    setSelectedOption(buttonValue)
+  }
+  useEffect(() => {
+    if (selectedOption) {
+      handleUpdate()
+    }
+  }, [selectedOption])
+
+  return (
+    <div className="major-search">
+      <h1 className="major-title">Program Search</h1>
+      <div className="major-search-container">
+        <SearchBox
+          placeHolder="Search for a Program"
+          list={searchPrograms}
+          value="Search"
+          onChange={selectHandler}
+        />
+        <button className="major-search-button" onClick={buttonHandler}>
+          Search
+        </button>
+      </div>
+      {showInfo && (
+        <div className="major-info-container">
+          <div /*className="major-info"*/>
+            <label className="major-name-label">Name: </label>
+            <input
+              type="text"
+              className="major-name"
+              defaultValue={selectedProgram.name}
+              onChange={(event) => {
+                setValue(event.target.value)
+              }}
+            />
+          </div>
+
+          <div>
+            <button /*className="major-update-button"*/ onClick={updator}>
+              Update
+            </button>
+          </div>
+          <div>
+            <button className="major-deactivate-button">Deactivate</button>
+          </div>
+        </div>
+      )}
+      {showPopup && (
+        <ConfPopUp
+          action="update"
+          onClose={handlePopUpClose}
+          onButtonClick={handlePopUpButtonClick}
+        />
+      )}
+      {showError && 
+      (<ErrorPopUp 
+        popUpContent={errorMessage}
+        onClose={handleErrorPopUpClose}
+      />)}
+    </div>
+  )
 }
 
 export default EditProgram
+
+
+/*          <div /*className="major-info"*//*>
+<label className="major-name-label">Credits: </label>
+<input
+  type="number"
+  className="major-name"
+  defaultValue={selectedProgram.credits}
+  onChange={(event) => {
+    setName(event.target.value)
+  }}
+/>
+</div>
+<div /*className="major-info"*//*>
+<label className="major-name-label">Elective Credits: </label>
+<input
+  type="number"
+  className="major-name"
+  defaultValue={selectedProgram.elective_credits}
+  onChange={(event) => {
+    setName(event.target.value)
+  }}
+/>
+</div> */

b0a749c3d62141a1b386a19ee6102d3d7da040ef
Bausav24 <92281958+Bausav24@users.noreply.github.com>
2023-05-21 22:21:59 -0700

    some css

--- frontend/src/pages/classes/class-add/class-add.jsx
+++ frontend/src/pages/classes/class-add/class-add.jsx
@@ -1,64 +1,64 @@
-import React, { useState, useEffect } from 'react'
-import './class-add.styles.scss'
-import ClassSelector from '../../../components/class-selector/class-selector'
-import axios from 'axios'
-import { useForm, Controller } from 'react-hook-form'
-import Switch from '@mui/material/Switch'
+import React, { useState, useEffect } from "react";
+import "./class-add.styles.scss";
+import ClassSelector from "../../../components/class-selector/class-selector";
+import axios from "axios";
+import { useForm, Controller } from "react-hook-form";
+import Switch from "@mui/material/Switch";
 
-import ErrorPopUp from '../../../components/PopUp/error/ErrorPopUp'
-import ConfPopUp from '../../../components/PopUp/confirmation/confPopUp'
+import ErrorPopUp from "../../../components/PopUp/error/ErrorPopUp";
+import ConfPopUp from "../../../components/PopUp/confirmation/confPopUp";
 
 const AddClass = () => {
-  const api_url = import.meta.env.VITE_API_URL
-  const [showPopup, setShowPopup] = useState(false)
-  const [selectedOption, setSelectedOption] = useState(null)
-  const [errorMessage, setErrorMesssage] = useState(' ')
-  const [showError, setShowError] = useState(false)
-  const { control, register, handleSubmit, setValue } = useForm()
+  const api_url = import.meta.env.VITE_API_URL;
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [errorMessage, setErrorMesssage] = useState(" ");
+  const [showError, setShowError] = useState(false);
+  const { control, register, handleSubmit, setValue } = useForm();
 
   const handleErrorPopUpClose = () => {
-    setShowError(false)
-  }
+    setShowError(false);
+  };
   const handlePopUpOpen = () => {
-    event.preventDefault()
-    setShowPopup(true)
-  }
+    event.preventDefault();
+    setShowPopup(true);
+  };
 
   const handlePopUpClose = () => {
-    setShowPopup(false)
-  }
+    setShowPopup(false);
+  };
   const handlePopUpButtonClick = (buttonValue) => {
-    setSelectedOption(buttonValue)
-  }
+    setSelectedOption(buttonValue);
+  };
   useEffect(() => {
     if (selectedOption) {
-      handleSubmit(addClass)()
-      setSelectedOption(false)
+      handleSubmit(addClass)();
+      setSelectedOption(false);
     }
-  }, [selectedOption])
+  }, [selectedOption]);
 
   const addClass = (data) => {
-    console.log(data)
+    console.log(data);
     axios
-      .post(api_url + 'Class.php', {
-        request: 'add_class',
+      .post(api_url + "Class.php", {
+        request: "add_class",
         user_id: 41792238,
         name: data.catalog,
         title: data.title,
         credits: data.credits,
-        fall: data.Fall ? 'Yes' : 'No',
-        spring: data.Spring ? 'Yes' : 'No',
-        winter: data.Winter ? 'Yes' : 'No',
-        summer: data.Summer ? 'Yes' : 'No'
+        fall: data.Fall ? "Yes" : "No",
+        spring: data.Spring ? "Yes" : "No",
+        winter: data.Winter ? "Yes" : "No",
+        summer: data.Summer ? "Yes" : "No",
       })
       .then((res) => {
-        console.log(res.data)
+        console.log(res.data);
       })
       .catch((error) => {
-        console.log(error)
-        console.log('no, here')
-      })
-  }
+        console.log(error);
+        console.log("no, here");
+      });
+  };
 
   return (
     <div className="form-container">
@@ -66,21 +66,21 @@ const AddClass = () => {
       <form onSubmit={handlePopUpOpen}>
         <div className="form-group">
           <label>Catalog Code:</label>
-          <input type="text" {...register('catalog')} placeholder="EX: CS481" />
+          <input type="text" {...register("catalog")} placeholder="EX: CS481" />
         </div>
 
         <div className="form-group">
           <label htmlFor="name">Name:</label>
           <input
             type="text"
-            {...register('title')}
+            {...register("title")}
             placeholder="EX Captstone Project"
           />
         </div>
 
         <div className="form-group">
           <label htmlFor="credits">Credits:</label>
-          <input type="text" {...register('credits')} />
+          <input type="text" {...register("credits")} />
         </div>
         <div>
           <label>Quarters Offered:</label>
@@ -95,10 +95,10 @@ const AddClass = () => {
                   <Switch
                     value={valueProp}
                     onChange={(event, val) => {
-                      setValue('Fall', val)
+                      setValue("Fall", val);
                     }}
                   />
-                )
+                );
               }}
             />
             <label>Winter</label>
@@ -111,10 +111,10 @@ const AddClass = () => {
                   <Switch
                     value={valueProp}
                     onChange={(event, val) => {
-                      setValue('Winter', val)
+                      setValue("Winter", val);
                     }}
                   />
-                )
+                );
               }}
             />
           </div>
@@ -129,10 +129,10 @@ const AddClass = () => {
                   <Switch
                     value={valueProp}
                     onChange={(event, val) => {
-                      setValue('Spring', val)
+                      setValue("Spring", val);
                     }}
                   />
-                )
+                );
               }}
             />
             <label>Summer</label>
@@ -145,16 +145,18 @@ const AddClass = () => {
                   <Switch
                     value={valueProp}
                     onChange={(event, val) => {
-                      setValue('Summer', val)
+                      setValue("Summer", val);
                     }}
                   />
-                )
+                );
               }}
             />
           </div>
         </div>
 
-        <button type="submit">Add Class</button>
+        <button className="btnAdd" type="submit">
+          Add Class
+        </button>
       </form>
 
       {showPopup && (
@@ -171,7 +173,7 @@ const AddClass = () => {
         />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default AddClass
+export default AddClass;

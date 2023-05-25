c7cea47ef48a97d3ed8c9ba2d8769cd5f3ba2207
Andrew Franco <andrewfrancomunoz101@gmail.com>
2023-05-24 18:24:23 -0700

    and for frontend

--- frontend/src/pages/enrollments/enrollment-page.jsx/enrollment-page.jsx
+++ frontend/src/pages/enrollments/enrollment-page.jsx/enrollment-page.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
-import './enrollment-page.style.scss'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function Enrollments() {
-  return <h1>This is enrollment page</h1>
-}
+const ClassTable = () => {
+  const [classData, setClassData] = useState([]);
 
-export default Enrollments
+  let api_url = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    axios
+      .post(api_url + "Class.php", { request: "all_active_classes" })
+      .then((res) => {
+        setClassData(res.data);
+        //setSearchClasses(res.data) ------>set searchClasses to all classes initially
+      });
+  }, []);
+
+  useEffect(() => {
+     console.log(classData)
+  }, [classData]);
+
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Class ID</th>
+            <th>Fall</th>
+            <th>Winter</th>
+            <th>Spring</th>
+            <th>Summer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {classData.map((classItem) => (
+            <tr key={classItem.id}>
+              <td>{classItem.name}</td>
+              <td>{classItem.fall}</td>
+              <td>{classItem.winter}</td>
+              <td>{classItem.spring}</td>
+              <td>{classItem.summer}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ClassTable;
